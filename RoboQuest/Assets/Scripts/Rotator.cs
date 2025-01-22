5db60995e958da9f74016423523169fa9a655774
JennyTran17 <teacupjt17@gmail.com>
2025-01-22 21:24:22 +0000

    Unity Project URP Upgrade
    I download URP package and change the rendering setting

--- RoboQuest/Assets/Scripts/Rotator.cs
+++ RoboQuest/Assets/Scripts/Rotator.cs
@@ -6,8 +6,13 @@ using UnityEngine.InputSystem;
 public class Rotator : MonoBehaviour
 {
     public float rotationSpeed = 100f;
+    private float playerSpeed = 10.0f;
     private Vector2 movement;
-    Rigidbody2D rb;
+    public int jumpImpulse = 7;
+    private int jumpCounter = 0;
+    public GroundCheck groundCheck;
+    public Rigidbody2D rb;
+    public GameObject planet;
 
     private void Start()
     {
@@ -17,23 +22,43 @@ public class Rotator : MonoBehaviour
     void Update()
     {
        
-        Debug.Log(movement);
+       //Debug.Log(rb.velocity);
     }
 
     void OnMove(InputValue movePosition)
     {
         //Get Player Position
         movement = movePosition.Get<Vector2>();
+ 
+    }
 
+    void OnJump()
+    {
+        // Trigger the Jump Animation
+        if (groundCheck.isGrounded == true)
+        {
+            
+            rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
+            jumpCounter = 0;
+            //animator.SetTrigger("Jump");
+        }
         
+        if (groundCheck.isGrounded != true && jumpCounter < 3)
+        {
+            
+            rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
+            jumpCounter += 1;
+           // animator.SetTrigger("Jump");
+        }
     }
 
     private void FixedUpdate()
     {
 
-        //Move the player
-        transform.Rotate(0, 0,  movement.x * rotationSpeed * Time.deltaTime);
-
+        //Move the planet
+        planet.transform.Rotate(0, 0,  movement.x * rotationSpeed * Time.deltaTime);
+        //player movement
+        rb.velocity = new Vector2((movement.x * playerSpeed), rb.velocity.y);
 
     }
 }
