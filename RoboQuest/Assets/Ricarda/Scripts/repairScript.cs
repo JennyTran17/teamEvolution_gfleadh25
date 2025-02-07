83d6fa3daf39f6386eb7e7fb2ba451f7b65e8237
Ricarda <ricardap18@gmail.com>
2025-02-07 12:39:15 +0000

    Skeleton for trigger repair mingame object
    Added scenes to build scenes, set up skeleton for trigger repair minigame object.

---
+++ RoboQuest/Assets/Ricarda/Scripts/repairScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class repairScript : GeneralInteraction
+{
+    private Dialogue dialogue;
+
+
+    public override void Interact()
+    {
+        //SceneManager.LoadScene("Sliding Puzzle", LoadSceneMode.Additive);
+
+    }
+}
