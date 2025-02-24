7ab2ebfe07035af46b955512607a8ab458e1b0c8
Daniel <>
2025-02-24 22:32:27 +0000

    Updated the puzzle game and the station
    - Daniel's stuff almost completed

--- RoboQuest/Assets/Daniel's Stuff/Scripts (upd)/Computers.cs
+++ RoboQuest/Assets/Daniel's Stuff/Scripts (upd)/Computers.cs
@@ -1,26 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Computers : GeneralInteraction
 {
-    //[SerializeField] private ComputerInteract computerInteract;
-    private ComputerInteract computerInteract;
-
-    void Awake()
-    {
-        computerInteract = GetComponent<ComputerInteract>();
-        if (computerInteract == null )
-        {
-            Debug.LogError("ComputerInteract component not found on " + gameObject.name);
-        }
-    }
+    [SerializeField] private ComputerInteract computerInteract;
 
     public override void Interact()
     {
-        if (computerInteract != null)
+        if (computerInteract.computerId == 1)
         {
-            computerInteract.InteractWithComputer();
+            Debug.Log("Opened Level 1");
+            SceneManager.LoadScene("Computer Level 1");
+            
+        }
+        else if (computerInteract.computerId == 2)
+        {
+            Debug.Log("Opened Level 2");
+            SceneManager.LoadScene("Computer Level 2");
+        }
+        else if (computerInteract.computerId == 3)
+        {
+            Debug.Log("Opened Level 3");
+            SceneManager.LoadScene("Computer Level 3");
         }
     }
 }
