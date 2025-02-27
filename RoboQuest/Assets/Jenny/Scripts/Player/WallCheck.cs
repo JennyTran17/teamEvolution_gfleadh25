b2a9e52756fe01d72883a91ea26e283de97eff66
Daniel <>
2025-02-27 07:02:23 +0000

    Fixed wall sticking issue

--- RoboQuest/Assets/Jenny/Scripts/Player/WallCheck.cs
+++ RoboQuest/Assets/Jenny/Scripts/Player/WallCheck.cs
@@ -4,28 +4,81 @@ using UnityEngine;
 
 public class WallCheck : MonoBehaviour
 {
-    public GameObject player;
+    //public GameObject player;
 
-    //public PlayerManager player;
+    ////public PlayerManager player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //}
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.CompareTag("Player"))
-        {
-            PlayerManager pm = collision.gameObject.GetComponent<PlayerManager>();
-            pm.movement.x = 0;
-        }
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if(collision.gameObject.CompareTag("Player"))
+    //    {
+    //        PlayerManager pm = collision.gameObject.GetComponent<PlayerManager>();
+    //        pm.movement.x = 0;
+    //    }
+    //}
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        // Access the player's movement script and disable horizontal movement
+    //        PlayerManager playerManager = collision.GetComponent<PlayerManager>();
+    //        Debug.Log("Accessed the Player Manager for WallCheck")
+    //        if (playerManager != null)
+    //        {
+    //            playerManager.DisableHorizontalMovement();
+    //        }
+    //    }
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        // Re-enable horizontal movement when the player leaves the wall
+    //        PlayerManager playerManager = collision.GetComponent<PlayerManager>();
+    //        if (playerManager != null)
+    //        {
+    //            playerManager.EnableHorizontalMovement();
+    //        }
+    //    }
+    //}
+
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.collider.CompareTag("Player"))
+    //    {
+    //        // Access the player's movement script and disable horizontal movement
+    //        PlayerManager playerManager = collision.collider.GetComponent<PlayerManager>();
+    //        if (playerManager != null)
+    //        {
+    //            playerManager.DisableHorizontalMovement();
+    //        }
+    //    }
+    //}
+
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (collision.collider.CompareTag("Player"))
+    //    {
+    //        // Re-enable horizontal movement when the player leaves the wall
+    //        PlayerManager playerManager = collision.collider.GetComponent<PlayerManager>();
+    //        if (playerManager != null)
+    //        {
+    //            playerManager.EnableHorizontalMovement();
+    //        }
+    //    }
+    //}
 }
