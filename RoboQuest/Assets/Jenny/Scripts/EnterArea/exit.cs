6ec05200b60032253ddc3fe81d55447ae64a427f
JennyTran17 <teacupjt17@gmail.com>
2025-02-28 19:24:42 +0000

    Ricarda's door anim

--- RoboQuest/Assets/Jenny/Scripts/EnterArea/exit.cs
+++ RoboQuest/Assets/Jenny/Scripts/EnterArea/exit.cs
@@ -5,11 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class exit : MonoBehaviour
 {
+  
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag.Equals("Player"))
+        if (collision.gameObject.tag.Equals("Player") && SceneManager.GetActiveScene().name.Equals("Ship"))
+        {
+            Animator animator = gameObject.GetComponent<Animator>();
+            animator.SetTrigger("open");
+        }
+
+        else if(collision.gameObject.tag.Equals("Player"))
         {
             SceneManager.LoadScene("Main Level");
+        }
+       
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Player") && SceneManager.GetActiveScene().name.Equals("Ship"))
+        {
+            Animator animator = gameObject.GetComponent<Animator>();
+            animator.SetTrigger("close");
         }
     }
 }
