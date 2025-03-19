10ccc4345754db4b348c68a147e43062477d3807
A00316647 (Ricarda) <ricardap18@gmail.com>
2025-03-19 16:53:13 +0000

    Prefab Pipes Started

--- RoboQuest/Assets/Ricarda/Scripts/PipeScript.cs
+++ RoboQuest/Assets/Ricarda/Scripts/PipeScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PipeScript : MonoBehaviour
 {
+    float[] rotations = { 0, 90, 180, 270 };
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int rand = Random.Range(0, rotations.Length);
+        transform.eulerAngles = new Vector3(0, 0, rotations[rand]);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit)
+            {
+                transform.Rotate(new Vector3(0, 0, 90));
+            }
+        }
     }
 }
